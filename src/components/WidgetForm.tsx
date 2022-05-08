691d194ec5e341c0947aafa3369ef5fa55a02364
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2022-05-07 23:57:52 -0300

    Feat:primeira parte do projeto concluido 1/5

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,7 @@
+export default function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 position-relative rounded-2xl mb-4 flex flex-col items-center drop-shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      Hello world!
+    </div>
+  );
+}
